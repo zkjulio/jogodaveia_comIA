03b7b6057c02c719651c95eb3e60f150de7e44a1
juliocosta98 <94931792+zkjulio@users.noreply.github.com>
2024-03-18 13:04:42 -0300

    Update player.py

--- tic-tac-toe-master/player.py
+++ tic-tac-toe-master/player.py
@@ -50,10 +50,10 @@ class SmartComputerPlayer(Player):
         return square
 
     def minimax(self, state, player):
-        max_player = self.letter  # yourself
+        max_player = self.letter  
         other_player = 'O' if player == 'X' else 'X'
 
-        # first we want to check if the previous move is a winner
+       
         if state.current_winner == other_player:
             return {'position': None, 'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (
                         state.num_empty_squares() + 1)}
@@ -61,17 +61,17 @@ class SmartComputerPlayer(Player):
             return {'position': None, 'score': 0}
 
         if player == max_player:
-            best = {'position': None, 'score': -math.inf}  # each score should maximize
+            best = {'position': None, 'score': -math.inf} 
         else:
-            best = {'position': None, 'score': math.inf}  # each score should minimize
+            best = {'position': None, 'score': math.inf}  
         for possible_move in state.available_moves():
             state.make_move(possible_move, player)
-            sim_score = self.minimax(state, other_player)  # simulate a game after making that move
+            sim_score = self.minimax(state, other_player)  
 
-            # undo move
+          
             state.board[possible_move] = ' '
             state.current_winner = None
-            sim_score['position'] = possible_move  # this represents the move optimal next move
+            sim_score['position'] = possible_move  
 
             if player == max_player:  # X is max player
                 if sim_score['score'] > best['score']:
